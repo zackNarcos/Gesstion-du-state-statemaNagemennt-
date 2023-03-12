6a617c6828595cdf194aa291a25526823add0beb
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-12 15:28:22 +0000

    add address routing as lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {ProductComponent} from "./pages/product/product.component";
 import {CartComponent} from "./pages/cart/cart.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'product/:id',
-    component: ProductComponent
-  },
-  {
-    path: 'cart',
-    component: CartComponent
-  },
+    // routes without lazy loading
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'product/:id',
+        component: ProductComponent
+    },
+    {
+        path: 'cart',
+        component: CartComponent
+    },
+
+    //routes with lazy loading
+    {
+        path: 'addresses',
+        loadChildren: () => import('./pages/address/address.module').then(m => m.AddressModule)
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
