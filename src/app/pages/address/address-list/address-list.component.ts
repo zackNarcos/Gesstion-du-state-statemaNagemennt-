947eb1334279a8c1a686464739a55dec80b1434d
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-12 18:39:43 +0000

    add address form

--- src/app/pages/address/address-list/address-list.component.ts
+++ src/app/pages/address/address-list/address-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NotiflixService} from "../../../shareds/services/notiflix.service";
 
 @Component({
   selector: 'app-address-list',
   templateUrl: './address-list.component.html',
   styleUrls: ['./address-list.component.scss']
 })
-export class AddressListComponent {
+export class AddressListComponent implements OnInit {
+
+  constructor(private notiflixService : NotiflixService) { }
+
+  ngOnInit() {
+    this.notiflixService.info('je suis un message de texte')
+  }
+
 
 }
