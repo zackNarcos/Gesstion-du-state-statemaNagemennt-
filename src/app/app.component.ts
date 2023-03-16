880f1b7a8508e7fb4ed84b8c395d3a634459e864
Ësse Jacques Dansomon <essedansomon@gmail.com>
2023-03-16 11:58:10 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {AppStateInterface} from "./store/app.state.interface";
-import {getCurrentRouteState} from "./store/store-route/router.selector";
 
 @Component({
     selector: 'app-root',
@@ -9,25 +6,16 @@ import {getCurrentRouteState} from "./store/store-route/router.selector";
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-    seriesId: string;
-    series;
-    private
-    subscriptions: { [key: string]: any } = {};
 
-    constructor(private store: Store<AppStateInterface>) {
+
+    constructor() {
     }
 
     ngOnInit(): void {
-        this.subscriptions['routerSelector'] = this.store
-            .pipe(select(getCurrentRouteState))
-            .subscribe((route: any) => {
-                const seriesId = route.params.seriesId;
-                this.series = this.series.find((series) => series.id === seriesId);
-            });
+
     }
 
     ngOnDestroy(): void {
-        this.subscriptions['routerSelector'].unsubscribe();
     }
 
 }
